baaca57b686555ee3dbc6b033186d50155d3dfa2
Veda Nadira KAnz Luay <vedanadirakanzluay@gmail.com>
2021-11-23 07:24:09 +0700

    Create DataModel.java

---
+++ Retrovolley/app/src/main/java/com/example/retrovolley/Model/DataModel.java
@@ -0,0 +1,38 @@
+package com.example.retrovolley.Model;
+
+public class DataModel {
+    private int id;
+    private String email, nama, alamat;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
